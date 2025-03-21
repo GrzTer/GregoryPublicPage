e685232b834ac35f920231cf5904c489bde40620
GrzTer <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-03-21 18:02:24 +0100

    Quartz sync: Mar 21, 2025, 6:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://gregorypublicpage.com",
+    baseUrl: "https://grzter.github.io/GregoryPublicPage/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
