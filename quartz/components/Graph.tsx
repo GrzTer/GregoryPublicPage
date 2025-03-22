4a2b460029e6e4b45671e772c9e2946dfde603f2
GrzTer <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-03-22 11:09:51 +0100

    vault backup: 2025-03-22 11:09:51

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -1,29 +1,30 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { useState } from "preact/hooks"; // or 'react/hooks' if using React
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types";
 // @ts-ignore
-import script from "./scripts/graph.inline"
-import style from "./styles/graph.scss"
-import { i18n } from "../i18n"
-import { classNames } from "../util/lang"
+import script from "./scripts/graph.inline";
+import style from "./styles/graph.scss";
+import { i18n } from "../i18n";
+import { classNames } from "../util/lang";
 
 export interface D3Config {
-  drag: boolean
-  zoom: boolean
-  depth: number
-  scale: number
-  repelForce: number
-  centerForce: number
-  linkDistance: number
-  fontSize: number
-  opacityScale: number
-  removeTags: string[]
-  showTags: boolean
-  focusOnHover?: boolean
-  enableRadial?: boolean
+  drag: boolean;
+  zoom: boolean;
+  depth: number;
+  scale: number;
+  repelForce: number;
+  centerForce: number;
+  linkDistance: number;
+  fontSize: number;
+  opacityScale: number;
+  removeTags: string[];
+  showTags: boolean;
+  focusOnHover?: boolean;
+  enableRadial?: boolean;
 }
 
 interface GraphOptions {
-  localGraph: Partial<D3Config> | undefined
-  globalGraph: Partial<D3Config> | undefined
+  localGraph: Partial<D3Config> | undefined;
+  globalGraph: Partial<D3Config> | undefined;
 }
 
 const defaultOptions: GraphOptions = {
@@ -57,15 +58,34 @@ const defaultOptions: GraphOptions = {
     focusOnHover: true,
     enableRadial: true,
   },
-}
+};
 
 export default ((opts?: Partial<GraphOptions>) => {
   const Graph: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const localGraph = { ...defaultOptions.localGraph, ...opts?.localGraph }
-    const globalGraph = { ...defaultOptions.globalGraph, ...opts?.globalGraph }
+    const [filterTag, setFilterTag] = useState<string>("");
+
+    // Merge filter into localGraph configuration:
+    const localGraph = {
+      ...defaultOptions.localGraph,
+      ...opts?.localGraph,
+      removeTags: filterTag ? [filterTag] : [],
+    };
+    const globalGraph = { ...defaultOptions.globalGraph, ...opts?.globalGraph };
+
     return (
       <div class={classNames(displayClass, "graph")}>
         <h3>{i18n(cfg.locale).components.graph.title}</h3>
+        {/* Filter UI */}
+        <div class="graph-filter">
+          <label htmlFor="graph-filter-input">Filter Tags: </label>
+          <input
+            id="graph-filter-input"
+            type="text"
+            placeholder="Enter tag to filter out"
+            value={filterTag}
+            onInput={(e: any) => setFilterTag(e.target.value)}
+          />
+        </div>
         <div class="graph-outer">
           <div class="graph-container" data-cfg={JSON.stringify(localGraph)}></div>
           <button class="global-graph-icon" aria-label="Global Graph">
@@ -99,11 +119,11 @@ export default ((opts?: Partial<GraphOptions>) => {
           <div class="global-graph-container" data-cfg={JSON.stringify(globalGraph)}></div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  Graph.css = style
-  Graph.afterDOMLoaded = script
+  Graph.css = style;
+  Graph.afterDOMLoaded = script;
 
-  return Graph
-}) satisfies QuartzComponentConstructor
+  return Graph;
+}) satisfies QuartzComponentConstructor;
