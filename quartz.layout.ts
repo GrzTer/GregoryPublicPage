e3df26c35a303283526735d984342e1e7bfee0d2
GrzTer <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-03-22 09:19:22 +0100

    vault backup: 2025-03-22 09:19:22

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/GrzTer",
+      "Discord Community": "https://discordapp.com/users/679760455056883869",
     },
   }),
 }
